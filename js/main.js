799ed0e9df28f542de2013352cdc09738b7ffe29
abir2665266 <dhar.abir.kumar@gmail.com>
2017-05-05 01:21:42 +0600

    class 07
    Staff_Section/OwlCarousel

---
+++ js/main.js
@@ -0,0 +1,31 @@
+(function($){
+    'use strict';
+    
+    
+    $(".staff_list").owlCarousel({
+        items : 4,
+        autoplay : false,
+        margin : 30,
+        loop : true,
+        nav : true,
+        navText : ["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"]
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+})(jQuery);
